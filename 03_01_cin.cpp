c4c70dc06bbb3755b4af757dc74695c19b79372c
Abhi <34265321+abhi25t@users.noreply.github.com>
2018-07-13 08:40:25 +0530

    Chapter_03

---
+++ 03_01_cin.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main() {
+	cout << "Please enter your first name (followed by 'enter'):\n";
+	string first_name; // first_name is a variable of type string
+	cin >> first_name; // read characters into first_name
+	cout << "Hello, " << first_name << "!\n";
+}
